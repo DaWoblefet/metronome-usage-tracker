f66e078d309f595bf2171b4567e2167453e717d2
Leonard Craft III <leonardcraft64@gmail.com>
2022-01-02 23:44:05 -0600

    Initial commit

---
+++ metronome-logic.js
@@ -0,0 +1,134 @@
+const ALL_METRONOME_NAMES = [
+    "Absorb","Accelerock","Acid","Acid Armor","Acid Spray","Acrobatics","Acupressure","Aerial Ace","Aeroblast","Agility","Air Cutter","Air Slash","Ally Switch","Amnesia","Anchor Shot","Ancient Power","Aqua Jet","Aqua Ring","Aqua Tail","Arm Thrust","Aromatherapy","Aromatic Mist","Assurance","Astonish","Attack Order","Attract","Aura Sphere","Aurora Beam","Aurora Veil","Autotomize","Avalanche","Baby-Doll Eyes","Baton Pass","Beat Up","Belly Drum","Bind","Bite","Blast Burn","Blaze Kick","Blizzard","Block","Blue Flare","Body Slam","Bolt Beak","Bolt Strike","Bone Rush","Bonemerang","Boomburst","Bounce","Brave Bird","Brick Break","Brine","Brutal Swing","Bubble Beam","Bug Bite","Bug Buzz","Bulk Up","Bulldoze","Bullet Punch","Bullet Seed","Burn Up","Burning Jealousy","Calm Mind","Charge","Charge Beam","Charm","Circle Throw","Clanging Scales","Clear Smog","Close Combat","Coaching","Coil","Confide","Confuse Ray","Confusion","Conversion","Conversion 2","Core Enforcer","Corrosive Gas","Cosmic Power","Cotton Guard","Cotton Spore","Court Change","Crabhammer","Cross Chop","Cross Poison","Crunch","Crush Claw","Crush Grip","Curse","Cut","Dark Pulse","Darkest Lariat","Dazzling Gleam","Defend Order","Defense Curl","Defog","Dig","Disable","Disarming Voice","Discharge","Dive","Doom Desire","Double Hit","Double Kick","Double Team","Double-Edge","Draco Meteor","Dragon Breath","Dragon Claw","Dragon Dance","Dragon Darts","Dragon Hammer","Dragon Pulse","Dragon Rush","Dragon Tail","Drain Punch","Draining Kiss","Dream Eater","Drill Peck","Drill Run","Dual Chop","Dual Wingbeat","Dynamic Punch","Earth Power","Earthquake","Echoed Voice","Eerie Impulse","Eerie Spell","Electric Terrain","Electrify","Electro Ball","Electroweb","Ember","Encore","Endeavor","Energy Ball","Entrainment","Eruption","Expanding Force","Explosion","Extrasensory","Extreme Speed","Facade","Fairy Lock","Fairy Wind","Fake Out","Fake Tears","False Swipe","Feather Dance","Fell Stinger","Fiery Dance","Final Gambit","Fire Blast","Fire Fang","Fire Lash","Fire Pledge","Fire Punch","Fire Spin","First Impression","Fishious Rend","Fissure","Flail","Flame Charge","Flame Wheel","Flamethrower","Flare Blitz","Flash Cannon","Flatter","Fling","Flip Turn","Floral Healing","Flower Shield","Fly","Flying Press","Focus Blast","Focus Energy","Force Palm","Forest's Curse","Foul Play","Freeze-Dry","Frenzy Plant","Frost Breath","Fury Attack","Fury Cutter","Fury Swipes","Fusion Bolt","Fusion Flare","Future Sight","Gastro Acid","Gear Grind","Gear Up","Geomancy","Giga Drain","Giga Impact","Glaciate","Glare","Grass Knot","Grass Pledge","Grassy Glide","Grassy Terrain","Gravity","Growl","Growth","Grudge","Guard Split","Guard Swap","Guillotine","Gunk Shot","Gust","Gyro Ball","Hail","Hammer Arm","Happy Hour","Harden","Haze","Head Charge","Head Smash","Headbutt","Heal Bell","Heal Pulse","Healing Wish","Heat Crash","Heat Wave","Heavy Slam","Hex","High Horsepower","High Jump Kick","Hold Back","Hone Claws","Horn Attack","Horn Drill","Horn Leech","Howl","Hurricane","Hydro Cannon","Hydro Pump","Hyper Beam","Hyper Voice","Hypnosis","Ice Beam","Ice Fang","Ice Punch","Ice Shard","Icicle Crash","Icicle Spear","Icy Wind","Imprison","Incinerate","Inferno","Infestation","Ingrain","Iron Defense","Iron Head","Iron Tail","Jaw Lock","Kinesis","Knock Off","Land's Wrath","Laser Focus","Lash Out","Last Resort","Lava Plume","Leaf Blade","Leaf Storm","Leaf Tornado","Leafage","Leech Life","Leech Seed","Leer","Lick","Light Screen","Liquidation","Lock-On","Lovely Kiss","Low Kick","Low Sweep","Lunar Dance","Lunge","Luster Purge","Mach Punch","Magic Coat","Magic Powder","Magic Room","Magical Leaf","Magma Storm","Magnet Rise","Magnetic Flux","Mean Look","Mega Drain","Mega Kick","Mega Punch","Megahorn","Memento","Metal Burst","Metal Claw","Metal Sound","Meteor Beam","Meteor Mash","Milk Drink","Mind Reader","Minimize","Mist","Mist Ball","Misty Explosion","Misty Terrain","Moonblast","Moonlight","Morning Sun","Mud Shot","Muddy Water","Mud-Slap","Multi-Attack","Mystical Fire","Nasty Plot","Night Daze","Night Shade","Night Slash","No Retreat","Noble Roar","Nuzzle","Oblivion Wing","Octazooka","Octolock","Outrage","Overheat","Pain Split","Parabolic Charge","Parting Shot","Pay Day","Payback","Peck","Perish Song","Petal Blizzard","Petal Dance","Phantom Force","Pin Missile","Play Nice","Play Rough","Pluck","Poison Fang","Poison Gas","Poison Jab","Poison Powder","Poison Sting","Poison Tail","Pollen Puff","Poltergeist","Pound","Powder Snow","Power Gem","Power Split","Power Swap","Power Trick","Power Trip","Power Whip","Power-Up Punch","Present","Prismatic Laser","Psybeam","Psych Up","Psychic","Psychic Fangs","Psychic Terrain","Psycho Cut","Psycho Shift","Psyshock","Psystrike","Purify","Quick Attack","Quiver Dance","Rain Dance","Rapid Spin","Razor Leaf","Razor Shell","Recover","Recycle","Reflect","Reflect Type","Rest","Retaliate","Revenge","Reversal","Rising Voltage","Roar","Roar of Time","Rock Blast","Rock Polish","Rock Slide","Rock Smash","Rock Throw","Rock Tomb","Rock Wrecker","Role Play","Rollout","Roost","Round","Sacred Fire","Sacred Sword","Safeguard","Sand Attack","Sand Tomb","Sandstorm","Scald","Scale Shot","Scary Face","Scorching Sands","Scratch","Screech","Searing Shot","Seed Bomb","Seismic Toss","Self-Destruct","Shadow Ball","Shadow Bone","Shadow Claw","Shadow Force","Shadow Punch","Shadow Sneak","Sheer Cold","Shell Side Arm","Shell Smash","Shift Gear","Shock Wave","Shore Up","Simple Beam","Sing","Skill Swap","Skitter Smack","Skull Bash","Sky Attack","Slack Off","Slam","Slash","Sleep Powder","Sludge","Sludge Bomb","Sludge Wave","Smack Down","Smart Strike","Smog","Smokescreen","Snipe Shot","Soak","Soft-Boiled","Solar Beam","Solar Blade","Spacial Rend","Spark","Sparkling Aria","Speed Swap","Spikes","Spirit Shackle","Spit Up","Spite","Splash","Spore","Stealth Rock","Steel Roller","Steel Wing","Sticky Web","Stockpile","Stomp","Stomping Tantrum","Stone Edge","Stored Power","Storm Throw","Strength","Strength Sap","String Shot","Struggle Bug","Stuff Cheeks","Stun Spore","Submission","Substitute","Sucker Punch","Sunny Day","Super Fang","Superpower","Supersonic","Surf","Swagger","Swallow","Sweet Kiss","Sweet Scent","Swift","Swords Dance","Synthesis","Tackle","Tail Slap","Tail Whip","Tailwind","Take Down","Tar Shot","Taunt","Tearful Look","Teatime","Teeter Dance","Teleport","Terrain Pulse","Thrash","Throat Chop","Thunder","Thunder Fang","Thunder Punch","Thunder Shock","Thunder Wave","Thunderbolt","Tickle","Topsy-Turvy","Torment","Toxic","Toxic Spikes","Tri Attack","Trick Room","Trick-or-Treat","Triple Axel","Triple Kick","Trop Kick","Twister","Uproar","U-turn","Vacuum Wave","Venom Drench","Venoshock","Vine Whip","Vise Grip","Vital Throw","Volt Switch","Volt Tackle","Water Gun","Water Pledge","Water Pulse","Water Shuriken","Water Spout","Waterfall","Weather Ball","Whirlpool","Whirlwind","Wild Charge","Will-O-Wisp","Wing Attack","Wish","Withdraw","Wonder Room","Wood Hammer","Work Up","Worry Seed","Wrap","X-Scissor","Yawn","Zap Cannon","Zen Headbutt","Zing Zap"
+];
+const ALL_METRONOME_IDS = [
+    "absorb","accelerock","acid","acidarmor","acidspray","acrobatics","acupressure","aerialace","aeroblast","agility","aircutter","airslash","allyswitch","amnesia","anchorshot","ancientpower","aquajet","aquaring","aquatail","armthrust","aromatherapy","aromaticmist","assurance","astonish","attackorder","attract","aurasphere","aurorabeam","auroraveil","autotomize","avalanche","babydolleyes","batonpass","beatup","bellydrum","bind","bite","blastburn","blazekick","blizzard","block","blueflare","bodyslam","boltbeak","boltstrike","bonerush","bonemerang","boomburst","bounce","bravebird","brickbreak","brine","brutalswing","bubblebeam","bugbite","bugbuzz","bulkup","bulldoze","bulletpunch","bulletseed","burnup","burningjealousy","calmmind","charge","chargebeam","charm","circlethrow","clangingscales","clearsmog","closecombat","coaching","coil","confide","confuseray","confusion","conversion","conversion2","coreenforcer","corrosivegas","cosmicpower","cottonguard","cottonspore","courtchange","crabhammer","crosschop","crosspoison","crunch","crushclaw","crushgrip","curse","cut","darkpulse","darkestlariat","dazzlinggleam","defendorder","defensecurl","defog","dig","disable","disarmingvoice","discharge","dive","doomdesire","doublehit","doublekick","doubleteam","doubleedge","dracometeor","dragonbreath","dragonclaw","dragondance","dragondarts","dragonhammer","dragonpulse","dragonrush","dragontail","drainpunch","drainingkiss","dreameater","drillpeck","drillrun","dualchop","dualwingbeat","dynamicpunch","earthpower","earthquake","echoedvoice","eerieimpulse","eeriespell","electricterrain","electrify","electroball","electroweb","ember","encore","endeavor","energyball","entrainment","eruption","expandingforce","explosion","extrasensory","extremespeed","facade","fairylock","fairywind","fakeout","faketears","falseswipe","featherdance","fellstinger","fierydance","finalgambit","fireblast","firefang","firelash","firepledge","firepunch","firespin","firstimpression","fishiousrend","fissure","flail","flamecharge","flamewheel","flamethrower","flareblitz","flashcannon","flatter","fling","flipturn","floralhealing","flowershield","fly","flyingpress","focusblast","focusenergy","forcepalm","forestscurse","foulplay","freezedry","frenzyplant","frostbreath","furyattack","furycutter","furyswipes","fusionbolt","fusionflare","futuresight","gastroacid","geargrind","gearup","geomancy","gigadrain","gigaimpact","glaciate","glare","grassknot","grasspledge","grassyglide","grassyterrain","gravity","growl","growth","grudge","guardsplit","guardswap","guillotine","gunkshot","gust","gyroball","hail","hammerarm","happyhour","harden","haze","headcharge","headsmash","headbutt","healbell","healpulse","healingwish","heatcrash","heatwave","heavyslam","hex","highhorsepower","highjumpkick","holdback","honeclaws","hornattack","horndrill","hornleech","howl","hurricane","hydrocannon","hydropump","hyperbeam","hypervoice","hypnosis","icebeam","icefang","icepunch","iceshard","iciclecrash","iciclespear","icywind","imprison","incinerate","inferno","infestation","ingrain","irondefense","ironhead","irontail","jawlock","kinesis","knockoff","landswrath","laserfocus","lashout","lastresort","lavaplume","leafblade","leafstorm","leaftornado","leafage","leechlife","leechseed","leer","lick","lightscreen","liquidation","lockon","lovelykiss","lowkick","lowsweep","lunardance","lunge","lusterpurge","machpunch","magiccoat","magicpowder","magicroom","magicalleaf","magmastorm","magnetrise","magneticflux","meanlook","megadrain","megakick","megapunch","megahorn","memento","metalburst","metalclaw","metalsound","meteorbeam","meteormash","milkdrink","mindreader","minimize","mist","mistball","mistyexplosion","mistyterrain","moonblast","moonlight","morningsun","mudshot","muddywater","mudslap","multiattack","mysticalfire","nastyplot","nightdaze","nightshade","nightslash","noretreat","nobleroar","nuzzle","oblivionwing","octazooka","octolock","outrage","overheat","painsplit","paraboliccharge","partingshot","payday","payback","peck","perishsong","petalblizzard","petaldance","phantomforce","pinmissile","playnice","playrough","pluck","poisonfang","poisongas","poisonjab","poisonpowder","poisonsting","poisontail","pollenpuff","poltergeist","pound","powdersnow","powergem","powersplit","powerswap","powertrick","powertrip","powerwhip","poweruppunch","present","prismaticlaser","psybeam","psychup","psychic","psychicfangs","psychicterrain","psychocut","psychoshift","psyshock","psystrike","purify","quickattack","quiverdance","raindance","rapidspin","razorleaf","razorshell","recover","recycle","reflect","reflecttype","rest","retaliate","revenge","reversal","risingvoltage","roar","roaroftime","rockblast","rockpolish","rockslide","rocksmash","rockthrow","rocktomb","rockwrecker","roleplay","rollout","roost","round","sacredfire","sacredsword","safeguard","sandattack","sandtomb","sandstorm","scald","scaleshot","scaryface","scorchingsands","scratch","screech","searingshot","seedbomb","seismictoss","selfdestruct","shadowball","shadowbone","shadowclaw","shadowforce","shadowpunch","shadowsneak","sheercold","shellsidearm","shellsmash","shiftgear","shockwave","shoreup","simplebeam","sing","skillswap","skittersmack","skullbash","skyattack","slackoff","slam","slash","sleeppowder","sludge","sludgebomb","sludgewave","smackdown","smartstrike","smog","smokescreen","snipeshot","soak","softboiled","solarbeam","solarblade","spacialrend","spark","sparklingaria","speedswap","spikes","spiritshackle","spitup","spite","splash","spore","stealthrock","steelroller","steelwing","stickyweb","stockpile","stomp","stompingtantrum","stoneedge","storedpower","stormthrow","strength","strengthsap","stringshot","strugglebug","stuffcheeks","stunspore","submission","substitute","suckerpunch","sunnyday","superfang","superpower","supersonic","surf","swagger","swallow","sweetkiss","sweetscent","swift","swordsdance","synthesis","tackle","tailslap","tailwhip","tailwind","takedown","tarshot","taunt","tearfullook","teatime","teeterdance","teleport","terrainpulse","thrash","throatchop","thunder","thunderfang","thunderpunch","thundershock","thunderwave","thunderbolt","tickle","topsyturvy","torment","toxic","toxicspikes","triattack","trickroom","trickortreat","tripleaxel","triplekick","tropkick","twister","uproar","uturn","vacuumwave","venomdrench","venoshock","vinewhip","visegrip","vitalthrow","voltswitch","volttackle","watergun","waterpledge","waterpulse","watershuriken","waterspout","waterfall","weatherball","whirlpool","whirlwind","wildcharge","willowisp","wingattack","wish","withdraw","wonderroom","woodhammer","workup","worryseed","wrap","xscissor","yawn","zapcannon","zenheadbutt","zingzap"
+];
+
+function toID(text) {
+    return text.toLowerCase().replace(/[^a-z0-9]/g, "");
+}
+
+function start() {
+    const newMoveInput = document.getElementById('new-move-input'); 
+    newMoveInput.addEventListener('keyup', function (e) {
+        if (e.key === 'Enter') {
+            checkValidMetronomeMove();
+        }
+    });
+
+    document.getElementById('new-move-confirm').addEventListener('click', checkValidMetronomeMove);
+    document.getElementById('reset-tracker').addEventListener('click', resetTrackerPrompt);
+
+    displayCurrentMetronomeResults();
+}
+
+function checkValidMetronomeMove() {
+    const newMoveInput = document.getElementById('new-move-input');
+    const failedInput = document.getElementById('failed-input');
+    const newMetronomeInput = newMoveInput.value;
+    const newMetronomeID = toID(newMetronomeInput);
+    const metronome_index = ALL_METRONOME_IDS.indexOf(newMetronomeID);
+    failedInput.classList.remove('hide');
+    if (metronome_index > -1) {
+        newMoveInput.value = '';
+        failedInput.classList.remove('red-border');
+        failedInput.classList.add('green-border');
+        failedInput.innerHTML = "✔️ " + ALL_METRONOME_NAMES[metronome_index] + " added!";
+        updateMetronomeStorage(metronome_index);
+    } else {
+        if (!failedInput.classList.contains('red-border')) {
+            failedInput.classList.remove('green-border');
+            failedInput.classList.add('red-border');
+            failedInput.innerHTML = "❌ " + newMetronomeInput + " is not a callable Metronome move.";
+        }
+    }
+}
+
+function updateMetronomeStorage(metronome_index) {
+    let metronomeStorage = JSON.parse(localStorage.getItem("metronome-usage-tracker"));
+    if (!metronomeStorage) {
+        metronomeStorage = {};
+        metronomeStorage[metronome_index] = 1;
+    } else {
+        const currentMoveCount = metronomeStorage[metronome_index];
+        if (currentMoveCount) { // it existed in the past
+            metronomeStorage[metronome_index] = metronomeStorage[metronome_index] + 1;
+        } else {
+            metronomeStorage[metronome_index] = 1;
+        }
+    }
+
+    localStorage.setItem("metronome-usage-tracker", JSON.stringify(metronomeStorage));
+    
+    displayCurrentMetronomeResults();
+}
+
+function displayCurrentMetronomeResults() {
+    let metronomeStorage = JSON.parse(localStorage.getItem("metronome-usage-tracker"));
+    let foundContent = '';
+    let notFoundContent = '';
+
+    if (metronomeStorage) {
+        foundContent = '<ul class="metronome-results" id="found-metronome-results">';
+        
+        const sortedMoves = sortFoundMoves(metronomeStorage);
+        sortedMoves.forEach(function(element) {
+            const count = element[1];
+            foundContent += '<li>' + ALL_METRONOME_NAMES[element[0]] + ' - found ' + count + ' time' + (count > 1 ? 's' : '') + '</li>';
+        });
+
+        // If not all moves have been found (typical case)
+        if (Object.keys(metronomeStorage).length !== ALL_METRONOME_NAMES.length) {
+            notFoundContent = '<ul class="metronome-results" id="not-found-metronome-results">';
+            ALL_METRONOME_NAMES.forEach(function(move, index) {
+                if (!metronomeStorage[index]) notFoundContent += '<li>' + move + '</li>';
+            });
+            notFoundContent += '</ul>';
+        }
+        foundContent += '</ul>';
+    } else { // no moves found at all (start)
+        notFoundContent = '<ul class="metronome-results" id="not-found-metronome-results">';
+        ALL_METRONOME_NAMES.forEach(function(move) {
+            notFoundContent += '<li>' + move + '</li>';
+        });
+        notFoundContent += '</ul>';
+    }
+
+    document.getElementById('found-metronome-results-container').innerHTML = foundContent;
+    document.getElementById('not-found-metronome-results-container').innerHTML = notFoundContent;
+
+    // Update the total counts too
+    document.getElementById('total-metronome-found-count-top').innerHTML = metronomeStorage ? Object.keys(metronomeStorage).length : 0;
+    document.getElementById('total-metronome-found-count').innerHTML = metronomeStorage ? Object.keys(metronomeStorage).length : 0;
+    document.getElementById('total-metronome-not-found-count').innerHTML = metronomeStorage ? ALL_METRONOME_NAMES.length - Object.keys(metronomeStorage).length : 0;
+}
+
+function sortFoundMoves(metronomeStorage) {
+    let keys = Object.keys(metronomeStorage);
+    let values = Object.values(metronomeStorage);
+    let sortedMoves = [];
+    for (i = 0; i < keys.length; i++) {
+        sortedMoves.push([keys[i], values[i]]);
+    }
+
+    sortedMoves.sort(function (a, b) {
+        if (a[1] - b[1] === 0) { // if the number of moves used was same
+            return a[0] - b[0]; // sort on name
+        } else {
+            return b[1] - a[1]; // else sort on number of moves used
+        }
+    });
+
+    return sortedMoves;
+}
+
+function resetTrackerPrompt() {
+    const willReset = confirm("Are you sure you want to reset the metronome usage tracker? All your progress will be deleted.");
+    if (willReset) {
+        localStorage.removeItem('metronome-usage-tracker');
+        displayCurrentMetronomeResults();
+    }
+}
+
+window.addEventListener('load', start);
